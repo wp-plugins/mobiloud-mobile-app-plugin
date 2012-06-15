09319d80007f1e006220d003a491f407a2219ae7
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-15 17:15:49 +0000

    tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@558658 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.2/post_html/js/mobiloud.js
@@ -0,0 +1,16 @@
+
+function mobiloud_orient()
+{
+  switch(window.orientation){  
+            case 0: document.getElementById("orient_css").href = "/wp-content/plugins/mobiloud/post_html/css/iphone_portrait.css";  
+            break;  
+
+            case -90: document.getElementById("orient_css").href = "/wp-content/plugins/mobiloud/post_html/css/iphone_landscape.css";  
+            break;  
+
+            case 90: document.getElementById("orient_css").href = "/wp-content/plugins/mobiloud/post_html/css/iphone_landscape.css";  
+            break;  
+  }
+}
+
+window.onload = mobiloud_orient(); 
