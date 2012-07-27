55387cb9aea9348fd4c7eee136434651db7b5fe7
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-27 22:01:15 +0000

    version 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@578241 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.2.3/post_html/ipad_html.php
@@ -0,0 +1,71 @@
+<?php
+function ipad_html($post)
+{
+	$prefiltered_html = str_replace("\n","<br>",$post->post_content);
+	
+	$html = str_get_html($post->post_content);	
+	
+	$img_tags = $html->find('img');
+	$iframe_tags = $html->find('iframe');
+	$object_tags = $html->find('object');
+	$embed_tags = $html->find('embed');
+	
+	$tags = array_merge($img_tags,$iframe_tags,$object_tags,$embed_tags);
+	$scripts = $html->find('script');
+	//on center, with specific width and no height
+	foreach($tags as $e)
+	{
+		//no width or height
+		if(isset($e->width)) $e->width = null;
+		if(isset($e->height)) $e->height = null;
+
+		$e->style = "max-width:520px;margin-top:20px;margin-bottom;20px;";
+		if($e->tag == "iframe" || $e->tag == "object" || $e->tag == "embed")
+		{
+			//should be a video
+				$e->width = 500;
+				$e->height = 300;
+		}
+		//center
+		$e->outertext = "<center><div class=\"mobiloud_media\">" . $e->outertext . "</div></center><p></p>";
+	}
+		
+	foreach($scripts as $s)
+	{
+		$s->outertext = ""; 
+	}
+	
+	//JAVASCRIPT INCLUDES
+	$header_js = "<script type=\"text/javascript\" src=\"/wp-content/plugins/mobiloud-mobile-app-plugin/post_html/js/jquery.min.js\"></script>";
+
+	$header_js .= "<script type=\"text/javascript\" src=\"/wp-content/plugins/mobiloud-mobile-app-plugin/post_html/js/mobiloud.js\"></script>";
+	//HEAD
+	$header = "<head>".$header_js;
+	
+	$header .= "<meta name=\"viewport\" content=\"width=device-width; minimum-scale=1.0; maximum-scale=1.0;\" />";
+	$header .= "<link rel=\"StyleSheet\" href=\"/wp-content/plugins/mobiloud-mobile-app-plugin/post_html/css/ipad.css\" type=\"text/css\"  media=\"screen\">";
+
+	$header .= "<link rel=\"StyleSheet\" href=\"/wp-content/plugins/mobiloud-mobile-app-plugin/post_html/css/ipad_portrait.css\" type=\"text/css\"  media=\"screen\" id=\"orient_css\">";
+	$header .= "</head>";
+
+	
+	$init_html = "<html>".$header;
+	
+	$spaces = "<p>&nbsp;</p>";
+	
+	$title = "<h1 class='title' align='left'>".$post->post_title."</h1>";
+	$title .= $spaces;
+	$title .= "<div class='ml_hr'></div>";
+	$title .= "<p></p>";
+	$title .= "<div class='author'>".get_author_name($post->post_author)."</div>";
+	$title .= "<p></p>";
+	$title .= "<div class='article_date'>".mysql2date('l, F j, Y',$post->post_date)."</div>";
+	$title .= "<p></p>";
+	$title .= "<div class='ml_hr'></div>";
+
+	$title .= $spaces;
+	
+	
+	return $init_html."<body><div id=\"content\">".$title.$html->save().$spaces."</div></body></html>";
+}
+?>
