dfeadf76786eacd9db6d0f29d6195075ae7343b8
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-19 19:23:27 +0000

    comment fixes
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@655390 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/comments/comment_iphone.php
+++ trunk/comments/comment_iphone.php
@@ -11,34 +11,31 @@ function ml_render_iphone_comment($comment)
 {
 	?>
 	<div class="ml_comment">
+		<table width="100%" border=0 cellspacing=5>
+			<tr valign="top">
+				<td>
+					<?php $uid_or_email = $comment->user_id != 0 ? $comment->user_id : $comment->comment_author_email;
+						  $link = ml_facebook_get_picture_url($uid_or_email);
+						  if($link) echo "<img src='$link' class='avatar avatar-50 photo' height=50 width=50>";
+						  else echo get_avatar($uid_or_email,50);
+					?>
+				</td>
+				<td class="ml_comment_body">
+					<div class="ml_comment_author">
+						<?php echo $comment->comment_author;?>
+					</div>			
+					<div class="ml_comment_text">
+						<?php echo ml_comment_content_to_html($comment->comment_content);?>
+					</div>
 
-		<div style="float:left;">
-			<div class="ml_comment_avatar">
-				<?php $uid_or_email = $comment->user_id != 0 ? $comment->user_id : $comment->comment_author_email;
-					  $link = ml_facebook_get_picture_url($uid_or_email);
-					  if($link) echo "<img src='$link' class='avatar avatar-50 photo' height=50 width=50>";
-					  else echo get_avatar($uid_or_email,50);
-				?>
-			</div>
-		</div>
-		<div style="float:left;margin-left:20px;">
-			<div class="ml_comment_author">
-				<?php echo $comment->comment_author;?>
-			</div>			
-			<div class="ml_comment_text">
-				<?php echo ml_comment_content_to_html($comment->comment_content);?>
-			</div>
-
-			<div class="ml_comment_bottom">
-				<div class="ml_comment_date">
-					<?php echo mysql2date('l, F j, Y. G:i',$comment->comment_date);?>				
-				</div>
-			</div>
-		</div>
-		
-		
-		<div style="clear:both;"></div>
-		
+					<div class="ml_comment_bottom">
+						<div class="ml_comment_date">
+							<?php echo mysql2date('l, F j, Y. G:i',$comment->comment_date);?>				
+						</div>
+					</div>
+				</td>
+			</tr>
+		</table>		
 	</div>
 	<?php
 }
