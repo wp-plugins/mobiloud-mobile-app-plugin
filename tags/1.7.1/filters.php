b53f6c30dacc8f02a562fadc2c8ac7b2ba23b2a0
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-24 22:21:37 +0000

    tag 1.7.1
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@658316 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.7.1/filters.php
@@ -0,0 +1,48 @@
+<?php 
+
+	class MobiloudFilter {
+
+
+		//filter function
+		public function filter($post_html)
+		{
+			return $post_html;
+		}
+
+	}
+
+	$ml_filters = Array();
+
+	function ml_filters_init() {
+		global $ml_filters;
+
+		$fd = fopen( dirname( __FILE__ )."/filters.txt","r");
+		if(!$fd) return false;
+		
+
+		while($line = fgets($fd)) {
+			$line = trim($line);
+			include_once dirname( __FILE__ )."/filters/".$line.".php";
+			$filter = new $line;
+			$ml_filters[] = $filter;
+		}
+
+		fclose($fd);
+		return true;
+	}
+
+	function ml_filters_get_filtered($post_html)
+	{
+		global $ml_filters;
+
+		foreach($ml_filters as $filter)
+		{
+			$post_html = $filter->filter($post_html);
+		}
+		return $post_html;
+	}
+
+
+	ml_filters_init();
+
+?>
