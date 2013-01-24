630aa7340bdf36a91a8f944ba17e724d73e34718
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-24 22:51:15 +0000

    version 1.7.2
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@658330 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.7.2/post_html/iphone_html.php
@@ -0,0 +1,61 @@
+<?php
+function iphone_html($post)
+{
+	$prefiltered_html = ml_filters_get_filtered($post->post_content);
+
+	$prefiltered_html = str_replace("\n","<p></p>",$prefiltered_html);
+ 	$prefiltered_html = preg_replace("/\[caption.*\"\]/", '', $prefiltered_html);
+ 	$prefiltered_html = preg_replace("/\[\/caption\]/", '', $prefiltered_html);
+
+	$html = str_get_html($prefiltered_html);	
+	
+	$img_tags = $html->find('img');
+	$iframe_tags = $html->find('iframe');
+	$object_tags = $html->find('object');
+	$embed_tags = $html->find('embed');
+	
+	$tags = array_merge($img_tags,$iframe_tags,$object_tags,$embed_tags);
+	$scripts = $html->find('script');
+	//on center, with specific width and no height
+	foreach($tags as $e)
+	{
+		//no width or height
+		if(isset($e->width)) $e->width = null;
+		if(isset($e->height)) $e->height = null;
+
+		$e->style = "max-width:280px;";
+		
+		//center
+		$e->outertext = "<center><div class=\"mobiloud_media\">" . $e->outertext . "</div></center><p></p>";
+	}
+		
+	foreach($scripts as $s)
+	{
+		$s->outertext = ""; 
+	}
+	
+	//JAVASCRIPT INCLUDES
+	$header_js = "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/jquery.min.js\"></script>";
+
+	$header_js .= "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/mobiloud.js\"></script>";
+	//HEAD
+	$header = "<head>".$header_js;
+	
+	$header .= "<meta name=\"viewport\" content=\"width=device-width; minimum-scale=1.0; maximum-scale=1.0;\" />";
+	$header .= "<link rel=\"StyleSheet\" href=\"".plugin_dir_url(__FILE__)."css/iphone.css\" type=\"text/css\"  media=\"screen\">";
+
+	$header .= "<link rel=\"StyleSheet\" href=\"".plugin_dir_url(__FILE__)."css/iphone_portrait.css\" type=\"text/css\"  media=\"screen\" id=\"orient_css\">";
+	$header .= "</head>";
+
+	
+	$init_html = "<html manifest=\"".plugin_dir_url(__FILE__+"../")."manifest.php\">".$header;
+	
+	$spaces = "<p>&nbsp;</p>";
+	
+	$title = "<h4 align='left'>".$post->post_title."</h4>";
+	$title .= "<table width='100%'><tr><td class='article_date' align=left>".mysql2date('l, F j, Y',$post->post_date)."</td><td class='author'  align=right>".get_author_name($post->post_author)."</td></tr></table><p>&nbsp;</p>";
+	
+	
+	return $init_html."<body onorientationchange=\"mobiloud_orient();\"><div id=\"content\"><p>&nbsp;</p>".$title.$html->save().$spaces."</div></body></html>";
+}
+?>
