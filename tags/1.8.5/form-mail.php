db6a38e5bde1319d416ad97904775b34c047507b
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-15 13:00:37 +0000

    add homepage for plugin
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@713439 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.8.5/form-mail.php
@@ -0,0 +1,16 @@
+<?php
+	$name = trim($_POST['contactName']);
+
+	$email = trim($_POST['email']);
+	
+	$website = trim($_POST['website']);
+
+	$emailTo = 'hello@mobiloud.com';
+	$subject = 'From '.$name;
+	$body = "New request from Mobiloud Plugin. \n\nName: $name \nEmail: $email \nWebsite: $website\n";
+	$headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+	
+	mail($emailTo, $subject, $body, $headers);
+	
+	header( 'Location: http://www.mobiloud.com/pricing.php?ref=plugin' ) ;
+?>
