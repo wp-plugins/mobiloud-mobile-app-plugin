e300d45e65f6fe0b7cc1cad6a9c98b914b1d3262
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-15 09:03:09 +0000

    add homepage for plugin
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/trunk@713317 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- homepage.php
+++ homepage.php
@@ -2,26 +2,69 @@
 
 function mobiloud_home_page()
 {
-	?>
+	if(isset($_POST['submitted'])) {
 	
-	<div></div>
+	$name = trim($_POST['contactName']);
 
+	$email = trim($_POST['email']);
+	
+	$website = trim($_POST['website']);
+
+	$emailTo = 'hello@mobiloud.com';
+	$subject = 'From '.$name;
+	$body = "New request from Mobiloud Plugin. \n\nName: $name \nEmail: $email \nWebsite: $website\n";
+	$headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+	
+	mail($emailTo, $subject, $body, $headers);
+
+	$emailSent = true;
+
+}
+ 
+	?>
 
 	<div class="wrap">
-		<div id="mobiloud_analytics_title" style="margin-top:20px;">
-			<img src="<?php echo MOBILOUD_PLUGIN_URL;?>/mobiloud_36.png" style="float:left;margin-top:5px;">
-			<h1 style="float:left;margin-left:20px;color:#555">Mobiloud homepage</h3>
-			<div style="clear:both;">
-		</div>
-
+		
 		<p>&nbsp;</p>
 		
 		<div class="narrow">
 
 			<!-- GENERAL -->
 			<div class="ml_homepage_general">
-				<div class='img_homepage'><img src="<?php echo MOBILOUD_PLUGIN_URL;?>/Design_you_trust1.png"></div>
-				<div class="content">This is the homepage</div>
+				<div class='img_homepage'><img src="<?php echo MOBILOUD_PLUGIN_URL;?>/app_overview.png"></div>
+				<div class="content center">
+					<h1>Turn your app into a stunning app</h1>
+					<h2>Enter your details below to get started</h2>
+					
+					<!-- FORM -->
+					<form class="form-horizontal formContact" target="_blank" action="<?php echo MOBILOUD_PLUGIN_URL;?>form-mail.php" id="contactForm" method="post">
+					  <div class="control-group inputGroup" >
+						<div class="controls">
+						  <label for="contactName">Your name</label>
+						  <input type="text" id="contactName" name="contactName" placeholder="Enter your name" required>
+						</div>
+					  </div>
+					  <div class="control-group inputGroup">
+						<div class="controls">
+						  <label for="email">Your email</label>
+						  <input type="email" id="email" name="email" placeholder="Enter your email" required>
+						</div>
+					  </div>
+					  <div class="control-group inputGroup">
+						<div class="controls">
+						  <label for="website">Your website</label>
+						  <input type="text" id="website" name="website" placeholder="Enter your website">
+						</div>
+					  </div>
+					  
+		  			  <input type="checkbox" name="terms" value="agree" required>I agree to Mobiloud's <a href="http://mobiloud.com/terms.php">terms of service</a> and <a href="https://www.iubenda.com/privacy-policy/435863/legal">privacy policy</a>
+		  			  <br /><br />
+		  			  
+					  <input type="submit" value="Get started now" id="submitted" name="submitted" class="btn-submit">
+					</form>
+					
+				</div>
+				
 			</div>
 		</div>
 
