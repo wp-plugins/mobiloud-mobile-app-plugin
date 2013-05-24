ffa064ee1bb120858e8a1afe27b8e0fbd8a71c68
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-24 09:37:40 +0000

    update 1.8.5
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@717720 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.8.5/form-mail.php
@@ -0,0 +1,25 @@
+ <html>
+
+	<body>
+		<?php
+			$name = trim($_POST['contactName']);
+
+			$email = trim($_POST['email']);
+	
+			$website = trim($_POST['website']);
+
+			$emailTo = 'sales@mobiloud.com';
+			$subject = 'New plugin installation from '.$website;
+			$body = "New request from Mobiloud Plugin. \n\nName: $name \nEmail: $email \nWebsite: $website\n";
+			$headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+	
+			mail($emailTo, $subject, $body, $headers);
+
+		?>
+
+		<script type="text/javascript">
+			window.location.href='http://www.mobiloud.com/pricing.php?ref=plugin';
+		</script>
+
+	</body>
+</html>
