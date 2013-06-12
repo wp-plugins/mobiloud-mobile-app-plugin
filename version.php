f21e073536c7b2ccf14da4b354070f6dc51bbbda
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-12 20:14:09 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/trunk@725876 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- version.php
+++ version.php
@@ -5,8 +5,5 @@
 				 "name" => get_bloginfo("name"),
 				 "plugin_dir_url" => plugin_dir_url(__FILE__));
 
-	global $wpdb;
-	$table_name = $wpdb->prefix . "mobiloud_pages";
-	
-	print_r($wpdb->get_col( "DESC " . $table_name, 0 ));
+	print_r($info);
 ?>
