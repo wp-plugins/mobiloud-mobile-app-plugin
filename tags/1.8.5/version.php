59e27e01c879d1bfb91bda437ec464d3058205d9
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-12 20:15:25 +0000

    update 1.8.5
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@725879 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.8.5/version.php
@@ -0,0 +1,9 @@
+<?php
+	include("../../../wp-blog-header.php");
+
+	$info = array("version" => "1.8.5",
+				 "name" => get_bloginfo("name"),
+				 "plugin_dir_url" => plugin_dir_url(__FILE__));
+
+	print_r($info);
+?>
