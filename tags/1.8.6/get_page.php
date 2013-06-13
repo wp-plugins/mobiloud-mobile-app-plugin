ced45bd4ccbd5f57fcb5abf4ecdec482d9a44ad7
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-13 11:16:31 +0000

    version 1.8.6
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@726189 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.8.6/get_page.php
@@ -0,0 +1,36 @@
+<?php
+include("../../../wp-blog-header.php");
+include_once("libs/img_resize.php");
+include_once("libs/ml_content_redirect.php");
+
+include_once("filters.php");
+include("post_html.php");
+
+$platform = $_GET["platform"];
+$page_ID = $_GET["page_ID"];
+
+if(!isset($platform) || $platform == NULL)
+	$platform = "iphone";
+
+$page = get_page($page_ID);
+if($page->post_content == NULL || strlen($page->post_content) == 0)
+{
+	//redirect
+	$link = get_permalink($page_ID);
+	header("Location: $link");
+	exit;
+}
+echo "<p>&nbsp;</p>";
+
+
+if($platform == "ipad")
+	echo ipad_html($page);
+else
+	echo iphone_html($page);
+
+echo "<p>&nbsp;</p>";
+echo "<p>&nbsp;</p>";
+echo "<p>&nbsp;</p>";
+echo "<p>&nbsp;</p>";
+
+?>
