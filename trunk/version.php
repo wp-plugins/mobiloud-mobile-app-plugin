700cfee38ef5e909d8a51f9000288b15052f36a8
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-16 22:31:44 +0000

    stable trunk
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@773666 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/version.php
+++ trunk/version.php
@@ -1,7 +1,7 @@
 <?php
 	include("../../../wp-blog-header.php");
 	global $wp_version, $required_php_version;
-	$info = array("version" => "1.8.12",
+	$info = array("version" => "1.8.14",
 				 "php_version" => phpversion(),
 				 "wp_version" => $wp_version,
 				 "required_php_version" => $required_php_version,
