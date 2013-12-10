334b2f72259b1ac11b6af5346355112111dc137d
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-10 16:02:31 +0000

    Pointer tooltip update
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@818319 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.9.0/admin_pointer.php
+++ tags/1.9.0/admin_pointer.php
@@ -21,8 +21,8 @@ function ml_enqueue_pointer_script_style( $hook_suffix ) {
 add_action('admin_enqueue_scripts','ml_enqueue_pointer_script_style');
 
 function ml_pointer_print_scripts() {
-	$pointer_content  = "<h3>Start Designing Your App!</h3>";
-  $pointer_content .= "<p>Design and preview your mobile app for free.</p>";
+	$pointer_content  = "<h3>Preview your mobile app now!</h3>";
+  $pointer_content .= "<p>Then access the configuration pages in the menu to further customise settings and add pages and categories to your app's menu.</p>";
   ?>
   <script type="text/javascript">
   jQuery(document).ready( function($) {
