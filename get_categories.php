1ed300df21e1fa3d5c593ffbf6e44c6763a8d734
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-14 20:49:33 +0000

    Added support for recursive hierarchical pages
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/trunk@914184 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- get_categories.php
+++ get_categories.php
@@ -3,6 +3,38 @@ include("../../../wp-blog-header.php");
 include_once("categories.php");
 include_once("pages.php");
 
+function build_page_object($dic){
+	
+	$childobject = array();
+	$childobject["title"] = $dic->post_title;
+	$childobject["link"] = get_permalink($dic->ID);
+	$childobject["ml_link"] = plugins_url("get_page.php?page_ID=".$dic->ID,__FILE__);
+	$childobject["ml_render"] = ml_page_get_render($dic->ID);
+	$childobject["id"] = "$dic->ID";
+	
+	$my_wp_query = new WP_Query();
+	$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
+
+	$children = get_page_children($dic->ID,$all_wp_pages);
+			
+	
+	$childarray = array();
+			
+	foreach($children as $child){
+						
+		if($child->post_title!=NULL&&$child->ID!=NULL&&$child->post_parent==$dic->ID){
+							
+			array_push($childarray,build_page_object($child));
+							
+		}
+						
+	}
+	
+	$childobject["children"] = $childarray;
+						
+	return $childobject;
+	
+}
 
 //ini_set('display_errors', 1);
 
@@ -49,15 +81,10 @@ foreach($pages as $p)
 			
 				foreach($children as $child){
 						
-						if($child->post_title!=NULL&&$child->ID!=NULL){
-							$childobject = array();
-							$childobject["title"] = $child->post_title;
-							$childobject["link"] = get_permalink($child->ID);
-							$childobject["ml_link"] = plugins_url("get_page.php?page_ID=".$child->ID,__FILE__);
-							$childobject["ml_render"] = ml_page_get_render($child->ID);
-							$childobject["id"] = "$child->ID";
-						
-							array_push($childarray,$childobject);
+						if($child->post_title!=NULL&&$child->ID!=NULL&&$child->post_parent==$p->ID){
+							
+							array_push($childarray,build_page_object($child));
+							
 						}
 						
 				}
@@ -72,4 +99,6 @@ foreach($pages as $p)
 
 
 echo json_encode(array("categories" => $final_categories, "pages" => $final_pages));
+
+
 ?>
