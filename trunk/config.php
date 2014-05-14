93c4288ec4f2296f42ec3d6350881738c098d868
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-14 01:35:11 +0000

    Added support for selecting content/full page for home page Added support for setting article list menu item title
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@913637 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/config.php
+++ trunk/config.php
@@ -20,11 +20,22 @@ $return_config["enable_dates"] = get_option('ml_article_list_enable_dates',true)
 if(get_option("ml_home_article_list_enabled",false)==true){
 	$return_config["home_page_type"] = "article_list";
 } else if(get_option("ml_home_page_enabled",false)==true){
+	
 	$return_config["home_page_type"] = "page";
 	$return_config["home_page_id"] = get_option("ml_home_page_id");
+	$return_config["home_page_full"] = get_option("ml_home_page_full");
+	
+	$return_config["show_article_list_menu_item"] = get_option("ml_show_article_list_menu_item",true);
+	$return_config["article_list_menu_item_title"] = get_option("ml_article_list_menu_item_title","Articles");
+	
+	
 } else if(get_option("ml_home_url_enabled",false)==true){
 	$return_config["home_page_type"] = "url";
 	$return_config["home_page_url"] = get_option("ml_home_url");
+	
+	$return_config["show_article_list_menu_item"] = get_option("ml_show_article_list_menu_item",true);
+	$return_config["article_list_menu_item_title"] = get_option("ml_article_list_menu_item_title","Articles");
+	
 }
 
 $return_config["enable_hierarchical_pages"] = get_option('ml_hierarchical_pages_enabled',true);
