cd4fb8d52d5ad21b1f648b6b440aa3eb1fffd392
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-18 09:20:34 +0000

    Removed Pushbots license check
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@934275 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.3.2/admin/subscriptions/page.php
@@ -0,0 +1,23 @@
+<script src="<?php echo MOBILOUD_PLUGIN_URL; ?>/admin/subscriptions/subscriptions.js" type="text/javascript">
+</script><div class="wrap">
+<div class="narrow">
+<div id="ml_admin_subscriptions">
+<h1>
+Membership Options
+</h1><?php if(ml_has_groups_library() == false) { ?>
+
+<div class="update-nag">
+You need to install and activate the
+<a href="http://www.itthinx.com/plugins/groups/" target="_blank">
+<b>
+WP Groups
+</b></a>plugin to enable password protected user logins to your app.
+</div><?php } else { ?>
+
+<div class="stuffbox" id="ml_admin_subscriptions_options">
+<h2 style="text-decoration:underline">
+Options
+</h2><h2>
+<input data-checked="<?php echo get_option('ml_subscriptions_enable',false); ?>" name="ml_subscriptions_enable" type="checkbox" />Require users to login to access the app
+</h2><input class="button button-primary button-large" data-label="Save" data-saving-label="Saving..." type="submit" value="Save" /></h3></div><?php } ?>
+</div></div></div>
---
+++ tags/2.3.2/src/admin/push_notifications/menu.php
@@ -0,0 +1,5 @@
+<?php
+	function mobiloud_push_notifications_page() {
+		include(dirname( __FILE__ ).'/menu_page.php');
+	}
+?>
