4dfcfe5c2e54648da8747229ee64df3fe07bb92f
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-19 09:45:27 +0000

    Added 2.3.3 tag
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@935007 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.3.3/src/subscriptions/login.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('display_errors', 1);
+include_once("../../../../wp-blog-header.php");
+include_once('functions.php');
+
+$data = array();
+$user = ml_login_wordpress('alvise','auguol');
+
+if(get_class($user) == "WP_User")
+{
+	//user
+	$group_user = new Groups_User($user->ID);
+	$data['user'] = array();
+	$data['user']['name'] = "$user->user_firstname $user->user_lastname";
+	$data['groups'] =  array();
+	$data['capabilities'] = array();
+
+	$groups = $group_user->__get('groups');
+	foreach($groups as $group) {
+		$g = array();
+		$g['id'] = $group->group_id;
+		$g['name'] = $group->name;
+		$data['groups'][] = $g;
+
+		//capabilities
+		$capabilities = $group->__get('capabilities');
+		if($capabilities != NULL) {
+			foreach($capabilities as $capability) {
+				$c = array();
+				$data['capabilities'][] = $capability->__get('capability');
+			}			
+		}
+	}
+}
+else {
+	//error
+}
+
+echo json_encode($data);
+?>
---
+++ tags/2.3.3/version.php
@@ -0,0 +1,11 @@
+<?php
+	$info = array("version" => "2.3.3");
+	$callback = $_GET['callback'];
+	if($callback) {
+		echo $callback."(";
+	}
+	echo json_encode($info);
+	if($callback) {
+		echo ")";
+	}
+?>
