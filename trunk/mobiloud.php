e63f83e8e4e27121ce0f4502157cd26f0738c4b3
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-19 09:44:00 +0000

    Updated to 2.3.3
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@935005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/mobiloud.php
+++ trunk/mobiloud.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Mobiloud
- * @version 2.3.2
+ * @version 2.3.3
  */
 /*
 Plugin Name: Mobiloud
 Plugin URI: http://www.mobiloud.com
 Description: Turn your WordPress site into beautiful native mobile apps. No coding required.
 Author: 50pixels
-Version: 2.3.2
+Version: 2.3.3
 Author URI: http://www.mobiloud.com
 */
 
@@ -19,7 +19,7 @@ define('MOBILOUD_PLUGIN_URL', plugins_url()."/mobiloud-mobile-app-plugin");
 define('MOBILOUD_PLUGIN_RELATIVE_URL',"/wp-content/plugins/mobiloud-mobile-app-plugin");
 
 
-define('MOBILOUD_PLUGIN_VERSION', "2.3.2");
+define('MOBILOUD_PLUGIN_VERSION', "2.3.3");
 define('MOBILOUD_PUSH_API_PUBLISH_URL', "https://push.mobiloud.com/api/notifications/publish");
 
 //define('MOBILOUD_POST_ASSETS_URL', "http://www.mobiloud.com/api/post");
