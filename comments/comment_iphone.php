5943fd84938815b90c262d5521a079999bee55a3
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-26 16:59:14 +0000

    =Fix post human dates and gravatar layout
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/trunk@939185 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- comments/comment_iphone.php
+++ comments/comment_iphone.php
@@ -3,6 +3,10 @@
 		return str_replace("\n","<br>",$content);
 	}
 
+    function get_avatar_url($uid_or_email, $size){
+        preg_match("/src='(.*?)'/i", get_avatar($uid_or_email, $size), $matches);
+        return $matches[1];
+    }
 
 	function ml_render_iphone_comment($comment){
 ?>
@@ -10,7 +14,10 @@
 		<?php
 			$uid_or_email = $comment->user_id != 0 ? $comment->user_id : $comment->comment_author_email;
 			$link = ml_facebook_get_picture_url($uid_or_email);
-			echo $link ? '<img src="' . $link . '" class="avatar avatar-50 photo">' : get_avatar($uid_or_email,50);
+			if(!$link){
+			    $link = get_avatar_url($uid_or_email, 50);
+            }
+			echo '<img src="' . $link . '" class="avatar avatar-50 photo">';
 		?>
 		<div class="comment_body">
 			<?php echo '<strong>' . $comment->comment_author . '</strong>: ' . nl2br($comment->comment_content); ?>
