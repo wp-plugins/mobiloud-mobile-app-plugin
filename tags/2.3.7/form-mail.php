710fc9fbd8069c70a14b41e335c641757567014a
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-04 20:21:03 +0000

    Added simulator
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@943532 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.3.7/form-mail.php
@@ -0,0 +1,32 @@
+<html>
+
+	<body>
+		<?php
+			$name = trim($_POST['contactName']);
+
+			$email = trim($_POST['email']);
+	
+			$website = trim($_POST['website']);
+
+			$root_url = $_POST['root_url'];
+			$plugins_url = $_POST['plugins_url'];
+			$appname = $_POST['appname'];
+		?>
+
+		<!-- FORM -->
+		<form  action="http://www.mobiloud.com/service/" id="configurator-form" method="get">
+			<input type='hidden' value="<?php echo $name?>" name='fullname'/>
+			<input type='hidden' value="<?php echo $email?>" name='email'/>
+			<input type='hidden' value="<?php echo $website?>" name='site'/>
+
+			<input type='hidden' value="<?php echo $root_url?>" name='root_url'/>
+			<input type='hidden' value="<?php echo $plugins_url?>" name='plugins_url'/>
+			<input type='hidden' value="<?php echo $appname?>" name='app_name'/>
+
+		</form>
+		<script type="text/javascript">
+			document.getElementById("configurator-form").submit();
+		</script>
+
+	</body>
+</html>
