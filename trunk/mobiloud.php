713c55b26e18158cf9690dcb57f07647ca00b150
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-04 22:02:09 +0000

    fix version mobiloud.php
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@943550 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/mobiloud.php
+++ trunk/mobiloud.php
@@ -8,7 +8,7 @@ Plugin Name: Mobiloud
 Plugin URI: http://www.mobiloud.com
 Description: Turn your WordPress site into beautiful native mobile apps. No coding required.
 Author: 50pixels
-Version: 2.3.4
+Version: 2.3.7
 Author URI: http://www.mobiloud.com
 */
 
