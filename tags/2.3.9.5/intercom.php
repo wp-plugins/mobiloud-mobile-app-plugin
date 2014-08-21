0c73faf499bc852c377f2c37033bbd625e34ed46
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-21 20:38:44 +0000

    Get vero will now only be done for a single admin
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@970073 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/2.3.9.5/intercom.php
+++ tags/2.3.9.5/intercom.php
@@ -1,21 +1,22 @@
-<?php 
+<?php    
 function ml_init_intercom() {
     
     //var_dump($user);exit;
 	if(is_admin() && current_user_can('administrator')) {
-		$user = wp_get_current_user();
+        
+		$user = get_tracking_user();
         ?>
             <script>(function(){var w=window;var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://api.intercom.io/api/js/library.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}})();</script>	
             <script id="IntercomSettingsScriptTag">
               window.intercomSettings = {
-                email: "<?php echo $user->user_email; ?>",
-                name: "<?php echo $user->user_firstname; ?> <?php echo $user->user_lastname; ?>",
-                site: "<?php echo site_url();?>",                
-                sitename: "<?php echo get_bloginfo('name'); ?>",
-                version: "<?php echo MOBILOUD_PLUGIN_VERSION;?>",
+                email: "<?php echo esc_js($user['email']); ?>",
+                name: "<?php echo esc_js($user['name']) ?>",
+                site: "<?php echo esc_js(site_url());?>",                
+                sitename: "<?php echo esc_js(get_bloginfo('name')); ?>",
+                version: "<?php echo esc_js(MOBILOUD_PLUGIN_VERSION);?>",
                 app_id: "h89uu5zu",
-                user_id: "<?php echo $user->user_email; ?>",
-                user_hash: "<?php echo hash_hmac("sha256", $user->user_email, "2d8ReoNHhovD4NhWCb72DgrghadvKVwGJsR0t6YR"); ?>",
+                user_id: "<?php echo esc_js($user['email']); ?>",
+                user_hash: "<?php echo esc_js(hash_hmac("sha256", $user['email'], "2d8ReoNHhovD4NhWCb72DgrghadvKVwGJsR0t6YR")); ?>",
                 widget: {
                     activator: "#intercom"
                 }
@@ -27,7 +28,7 @@ function ml_init_intercom() {
 
 function ml_init_getvero() {    
     if(is_admin() && current_user_can('administrator')) {
-        $user = wp_get_current_user();
+        $user = get_tracking_user();
         ?>
         <script type="text/javascript">
             var _veroq = _veroq || [];
@@ -39,13 +40,13 @@ function ml_init_getvero() {
             } ]);
 
             _veroq.push(['user', {
-                id: "<?php echo $user->user_email; ?>", 
-                email: "<?php echo $user->user_email; ?>", 
-                name: "<?php echo $user->user_firstname; ?> <?php echo $user->user_lastname; ?>",
-                website: "<?php echo site_url(); ?>",
-                website_name: "<?php echo get_bloginfo('name'); ?>",
+                id: "<?php echo esc_js($user['email']); ?>", 
+                email: "<?php echo esc_js($user['email']); ?>", 
+                name: "<?php echo esc_js($user['name']); ?>",
+                website: "<?php echo esc_js(site_url()); ?>",
+                website_name: "<?php echo esc_js(get_bloginfo('name')); ?>",
                 user_lever: 'administrator',
-                version: "<?php echo MOBILOUD_PLUGIN_VERSION;?>",
+                version: "<?php echo esc_js(MOBILOUD_PLUGIN_VERSION);?>",
               }]);
 
             (function() {var ve = document.createElement('script'); ve.type = 'text/javascript'; ve.async = true; ve.src = '//getvero.com/assets/m.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ve, s);})();
@@ -78,8 +79,22 @@ function ml_track_intercom($action, $loadInit=false) {
         ?>
         <script type="text/javascript">
             Intercom("trackUserEvent", "<?php echo $action; ?>");
-        </script>
+        </script> 
         <?php
     }
+}
+
+function get_tracking_user() {
+    $tracking_user = get_option('ml_tracking_user', false);
+    if(!$tracking_user) {
+        $user = wp_get_current_user();
+        
+        $tracking_user = array(
+            'email'=>$user->user_email,
+            'name'=>trim($user->user_firstname . ' ' . $user->user_lastname)
+        );
+        update_option('ml_tracking_user', $tracking_user);        
+    }     
+    return $tracking_user;
 }
 ?>
