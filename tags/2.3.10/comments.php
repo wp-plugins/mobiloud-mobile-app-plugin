77e84dc64785e94f7a04b51bf3e427205ec9252c
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-05 22:30:43 +0000

    Updated to 2.3.10
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@982478 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.3.10/comments.php
@@ -0,0 +1,31 @@
+<?php
+include("../../../wp-blog-header.php");
+ini_set('display_errors', 0);
+
+include("comments/comment_iphone.php");
+
+function ml_render_comment($comment, $platform="iphone"){
+	if($platform == "iphone"){
+		ml_render_iphone_comment($comment);
+	}
+}
+
+function ml_render_comments($post_id, $platform="iphone", $offset=0){
+	$parameters = array(
+		'post_id' => $post_id,
+		//'number' => 10,
+		'offset' =>  $offset,
+		'status' => "approve",
+		'order' => 'ASC'
+	);
+
+	$comments = get_comments($parameters);
+	foreach($comments as $comment){
+		ml_render_comment($comment,$platform);
+	}
+}
+
+?><!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<link href="<?php echo plugins_url('mobiloud-mobile-app-plugin/comments/css/styles.css'); ?>" rel="stylesheet" media="all" /><link href="<?php echo plugins_url('mobiloud-mobile-app-plugin/comments/css/_typeplate.css'); ?>" rel="stylesheet" media="all" /></head><body>
+<?php ml_render_comments(sanitize_text_field($_GET['post_id'])); ?>
+</body></html>
---
+++ tags/2.3.10/version.php
@@ -0,0 +1,12 @@
+<?php
+include("../../../wp-load.php");
+	$info = array("version" => "2.3.10");
+	$callback = sanitize_text_field($_GET['callback']);
+	if($callback) {
+		echo $callback."(";
+	}
+	echo json_encode($info);
+	if($callback) {
+		echo ")";
+	}
+?>
