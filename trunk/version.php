b87ca0c1767bcf680f7366222fc6597620c8fae1
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-05 22:23:55 +0000

    2.3.10 - Disable New Relic in post JSON
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@982473 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/version.php
+++ trunk/version.php
@@ -1,6 +1,6 @@
 <?php
 include("../../../wp-load.php");
-	$info = array("version" => "2.3.9.8");
+	$info = array("version" => "2.3.10");
 	$callback = sanitize_text_field($_GET['callback']);
 	if($callback) {
 		echo $callback."(";
