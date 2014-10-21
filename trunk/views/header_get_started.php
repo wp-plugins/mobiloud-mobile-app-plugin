0c062c1187c6ae0c3398f6ef6db4676b3eed5307
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-21 08:34:52 +0000

    reverting to 2.3.10 version
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@1011260 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/views/header_get_started.php
+++ trunk/views/header_get_started.php
@@ -1,6 +1,8 @@
 <div id="ml-sub-header">
-    <div class="ml-intro-text">Use the plugin to <strong>customize and manage your own app</strong>. You can test your app for free and <a href="http://www.mobiloud.com/pricing/" target="_blank">sign up</a> when you're ready to publish it your app.</p>
-        <p>For more information on Mobiloud, read <a href="http://www.mobiloud.com/how-it-works/" target="_blank">How It Works</a>, our <a href="http://www.mobiloud.com/features/" target="_blank">Features Tour</a> and for any questions see the <a href="http://www.mobiloud.com/help/" target="_blank">Help &amp; Support site</a>.</p>
+    <div class="ml-intro-text">
+		<p>
+		Mobiloud lets you easily publish your own mobile apps. Start by customizing the design, configuring the menu and testing your app on your device.</p>
+        <p>For more information, see <a href="http://www.mobiloud.com/how-it-works/" target="_blank">How It Works</a>, our <a href="http://www.mobiloud.com/features/" target="_blank">Features Tour</a> and for any questions see the <a href="http://www.mobiloud.com/help/" target="_blank">Help &amp; Support site</a>.</p>
     </div>
     <?php if(strlen(Mobiloud::get_option('ml_pb_app_id')) <= 0 && strlen(Mobiloud::get_option('ml_pb_secret_key')) <= 0): ?>
     <div class="ml-task-list">
