5b36e6639abfb3c598f587566c9c6aacad5d7554
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-21 23:18:03 +0000

    3.0.5 update with bug fixes and admob configuration
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin@1011815 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.0.5/views/push_settings.php
@@ -0,0 +1,47 @@
+<div id="ml_push_settings" class="tabs-panel ml-compact">
+    <form method="post" action="<?php echo admin_url('admin.php?page=mobiloud_push&tab=settings'); ?>">
+        <?php wp_nonce_field('form-push_settings'); ?>        
+        <h4>Automatic push notifications</h4>
+        <div class='ml-col-row'>
+            <div class='ml-col-half'>
+                <p>Automatically send push notifications when a new post is published</p>            
+            </div>
+            <div class='ml-col-half'>
+                <div class="ml-form-row ml-checkbox-wrap">
+                    <input type="checkbox" id="ml_push_notification_enabled" name="ml_push_notification_enabled" value="true" <?php echo Mobiloud::get_option('ml_push_notification_enabled') ? 'checked' : ''; ?>/>
+                    <label for="ml_push_notification_enabled">Send notifications automatically</label>
+                </div>
+                <p>Select which categories will generate a push notification (leave empty for all)</p>
+                <select id="ml_push_notification_categories" name='ml_push_notification_categories[]' data-placeholder="Select Categories..." style="width:350px;" multiple class="chosen-select">
+                    <option></option>
+                    <?php $categories = get_categories();?>
+                    <?php $pushCategories = ml_get_push_notification_categories(); ?>
+                    <?php 
+                        foreach($categories as $c) {
+                            $selected = '';
+                            if(is_array($pushCategories) && count($pushCategories) > 0) {
+                                foreach($pushCategories as $pushCategory) {
+                                    if($pushCategory->cat_ID == $c->cat_ID) {
+                                        $selected = 'selected';
+                                    }
+                                }
+                            }
+                            echo "<option value='$c->cat_ID' $selected>$c->cat_name</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+        </div>
+        <h4>Security settings (advanced)</h4>
+        <div class='ml-col-row'>
+            <div class='ml-col-half'>
+                <p>Choose whether to use SSL to communicate with our push service.</p>            
+            </div>
+            <div class="ml-form-row ml-checkbox-wrap no-margin">
+                <input type="checkbox" id="ml_pb_use_ssl" name="ml_pb_use_ssl" value="true" <?php echo Mobiloud::get_option('ml_pb_use_ssl') ? 'checked' : ''; ?>/>
+                <label for="ml_pb_use_ssl">Enable SSL for push notifications</label>
+            </div>
+        </div>
+        <p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
+    </form>
+</div>
