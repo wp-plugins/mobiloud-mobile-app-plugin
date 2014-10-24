8f7d955af405180be59575bda66b33df541f6900
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-24 12:37:16 +0000

    3.0.6 update
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/tags/3.0.6@1013357 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post/body_content.php
+++ post/body_content.php
@@ -1,5 +1,5 @@
 <?php
-    
+
     if(!function_exists('setup_postdata_custom')) {
         /**
         * Set up global post data.
@@ -65,8 +65,8 @@
 
     echo stripslashes(get_option('ml_banner_above_content', ''));
 	eval(stripslashes(get_option('ml_post_start_body')));
-	echo stripslashes(get_option('ml_html_post_start_body'));    
-    
+	echo stripslashes(get_option('ml_html_post_start_body'));
+
 	eval(stripslashes(get_option('ml_post_before_top_banner')));
 
 	// featured image banner
@@ -79,41 +79,41 @@
 	eval(stripslashes(get_option('ml_post_after_top_banner')));
 
 ?>
-<article>
+<article class="mb_article">
 <?php
 
 	eval(stripslashes(get_option('ml_post_before_details')));
 	echo stripslashes(get_option('ml_html_post_before_details'));
 
-if(!isset($_GET["page_ID"])){
-	// title, date, author, meta
-	echo get_option('ml_post_date_enabled') ? '<div class="post_meta"><time title="' . $post->post_date . '">' . human_time_diff(strtotime($post->post_date), time()) . '</time></div>' : '';
+	if(!isset($_GET["page_ID"])){
+		// title, date, author, meta
+		echo get_option('ml_post_date_enabled') ? '<p class="mb_post_meta"><time title="' . $post->post_date . '">' . date_i18n(get_option('date_format'), strtotime($post->post_date)) . '</time></p>' : '';
 
-} else {
-	echo get_option('ml_page_date_enabled') ? '<div class="post_meta"><time title="' . $post->post_date . '">' . human_time_diff(strtotime($post->post_date), time()) . '</time></div>' : '';
-}
-
-	echo '<div class="post_meta right">'; eval(stripslashes(get_option('ml_post_right_of_date'))); echo '</div>';
+	} else {
+		echo get_option('ml_page_date_enabled') ? '<p class="mb_post_meta"><time title="' . $post->post_date . '">' . date_i18n(get_option('date_format'), strtotime($post->post_date)) . '</time></p>' : '';
+	}
+	
+	echo '<div class="mb_post_meta right">'; eval(stripslashes(get_option('ml_post_right_of_date'))); echo '</div>';
 ?>
-	<div class="clear"></div>
+	<div class="mb_clear"></div>
     <?php echo stripslashes(get_option('ml_banner_above_title', '')); ?>
     <?php if(!isset($_GET["page_ID"])): ?>
         <?php if(get_option('ml_post_title_enabled')): ?>
-            <h1 class="gamma post_title"><?php echo $post->post_title; ?></h1>
+            <h1 class="gamma mb_post_title"><?php echo $post->post_title; ?></h1>
         <?php endif; ?>
     <?php else: ?>
         <?php if(get_option('ml_page_title_enabled')): ?>
-            <h1 class="gamma post_title"><?php echo $post->post_title; ?></h1>
+            <h1 class="gamma mb_post_title"><?php echo $post->post_title; ?></h1>
         <?php endif; ?>
-    <?php endif; ?>      
+    <?php endif; ?>
 <?php
 
 	if( !isset($_POST['allow_lazy']) || isset($_GET['fullcontent']) || get_option('ml_eager_loading_enable')=='true' || isset($_GET["page_ID"]) || isset($_POST['post_id'])){
 
 		if(!isset($_GET["page_ID"])){
-			echo get_option('ml_post_author_enabled') ? '<p class="post_meta">' . get_the_author_link() . '</p><div class="clear"></div>' : ''; // clear because .post_meta is floated
+			echo get_option('ml_post_author_enabled') ? '<p class="mb_post_meta">' . get_the_author_link() . '</p><div class="mb_clear"></div>' : ''; // clear because .post_meta is floated
 		} else {
-			echo get_option('ml_page_author_enabled') ? '<p class="post_meta">' . get_the_author_link() . '</p><div class="clear"></div>' : ''; // clear because .post_meta is floated
+			echo get_option('ml_page_author_enabled') ? '<p class="mb_post_meta">' . get_the_author_link() . '</p><div class="mb_clear"></div>' : ''; // clear because .post_meta is floated
 		}
 
 	eval(stripslashes(get_option('ml_post_after_details')));
