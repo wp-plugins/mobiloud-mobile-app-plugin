c2c0e17b7d0af547a44ad3e9ab6f09452eec9107
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-20 21:36:51 +0000

    Amended text truncation on app preview
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/trunk@1029679 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/app_preview.php
+++ views/app_preview.php
@@ -41,7 +41,7 @@
                                 <img class='ml-preview-img' src='<?php echo $imageUrl; ?>'/>   
                             <?php endif; ?>
                             <div class='ml-preview-article-body' <?php echo!$imageUrl ? 'style="width:100%;"' : ''; ?>>
-                                <h3><?php echo Mobiloud_App_Preview::trim_post_title($post->post_title, get_option('ml_article_list_view_type', 'extended') == 'extended' ? null : 50); ?></h3>
+                                <h3><?php echo Mobiloud_App_Preview::trim_post_title($post->post_title, get_option('ml_article_list_view_type', 'extended') == 'extended' ? null : 45); ?></h3>
                                 <span class='ml-article-date'><?php echo Mobiloud_App_Preview::how_long_ago(strtotime($post->post_date)); ?></span>
                             </div>
                         </div>
