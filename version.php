c8155930d45bb7be5f88576171edd88ac0af9500
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 19:18:56 +0000

    3.1.5 Update with minor fixes
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/tags/3.1.5@1092719 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 include("../../../wp-load.php");
-	$info = array("version" => "3.1.4");
+	$info = array("version" => "3.1.5");
 	$callback = sanitize_text_field($_GET['callback']);
 	if($callback) {
 		echo $callback."(";
