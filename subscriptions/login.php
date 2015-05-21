f603e84d005d382fa6d709b822b134568e306d51
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 17:00:25 +0000

    First, revert Posts endpoint (posts.php) to v3.1.8 Then, add support for category_permalink option in Posts endpoint In login, only perform Groups plugin capabilities check if the plugin is determined to be present. Deliver default WP capabilities when it's not.
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/trunk@1165119 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subscriptions/login.php
+++ subscriptions/login.php
@@ -11,29 +11,35 @@ $user = ml_login_wordpress($username,$password);
 
 if(get_class($user) == "WP_User")
 {
-	//user
-	$group_user = new Groups_User($user->ID);
-	$data['user'] = array();
-	$data['user']['name'] = "$user->user_firstname $user->user_lastname";
-	$data['groups'] =  array();
-	$data['capabilities'] = array();
 
-	$groups = $group_user->__get('groups');
-	foreach($groups as $group) {
-		$g = array();
-		$g['id'] = $group->group_id;
-		$g['name'] = $group->name;
-		$data['groups'][] = $g;
+	// Get capabilities from Groups plugin if it's present
+	if (class_exists('Groups_User')) {
 
-		//capabilities
-		$capabilities = $group->__get('capabilities');
-		if($capabilities != NULL) {
-			foreach($capabilities as $capability) {
-				$c = array();
-				$data['capabilities'][] = $capability->__get('capability');
-			}			
+		$group_user = new Groups_User($user->ID);
+		$data['user'] = array();
+		$data['user']['name'] = "$user->user_firstname $user->user_lastname";
+		$data['groups'] =  array();
+		$data['capabilities'] = array();
+
+		$groups = $group_user->__get('groups');
+		foreach($groups as $group) {
+			$g = array();
+			$g['id'] = $group->group_id;
+			$g['name'] = $group->name;
+			$data['groups'][] = $g;
+
+			//capabilities
+			$capabilities = $group->__get('capabilities');
+			if($capabilities != NULL) {
+				foreach($capabilities as $capability) {
+					$data['capabilities'][] = $capability->__get('capability');
+				}			
+			}
 		}
+	} else { // Default WP capabilities
+		$data['capabilities'] = $user->allcaps;
 	}
+
 }
 else {
 	//error
