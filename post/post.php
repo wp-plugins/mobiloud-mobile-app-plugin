b5b52ae5a2eccf4158a73124d6058595c085c721
psac50 <psac50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-20 15:33:50 +0000

    Fix RTL
    git-svn-id: https://plugins.svn.wordpress.org/mobiloud-mobile-app-plugin/trunk@1202377 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post/post.php
+++ post/post.php
@@ -20,9 +20,10 @@
 		header("HTTP/1.1 404 Not Found");
 	}
 
-	$ltr_rtl = get_option('ml_rtl_text_enable') == 'true' ? 'rtl' : 'ltr';
+	$ltr_rtl = get_option('ml_rtl_text_enable') == '1' ? 'rtl' : 'ltr';
 
-?><!DOCTYPE html><html><head dir="<?php echo $ltr_rtl; ?>"><meta charset="utf-8"><meta name="description" content=""><meta name="keywords" content=""><meta name="language" content="en"/><meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, user-scalable=no">
+
+?><!DOCTYPE html><html dir="<?php echo $ltr_rtl; ?>"><head><meta charset="utf-8"><meta name="description" content=""><meta name="keywords" content=""><meta name="language" content="en"/><meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, user-scalable=no">
 <link href="<?php echo plugins_url('mobiloud-mobile-app-plugin/post/css/styles.css'); ?>" rel="stylesheet" media="all" /><link href="<?php echo plugins_url('mobiloud-mobile-app-plugin/post/css/_typeplate.css'); ?>" rel="stylesheet" media="all" />
 <?php
 	$custom_css = stripslashes(get_option('ml_post_custom_css'));
